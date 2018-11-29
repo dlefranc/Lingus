d447af762622d22142ef1e47123b4e2de53eeb74
dlefranc <dlefranc@scinfe177.univ-pau.fr>
2018-11-29 09:34:18 +0100

    Avance windows

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,18 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.text.Font;
+import javafx.fxml.JavaFXBuilderFactory;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import demo.model.User;
+import demo.security.Authenticator;
 
 public class Main extends Application {
 	
@@ -28,7 +35,33 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	//////////////////////////////////////////////////////////////////
+	private Parent replaceSceneContent(String fxml) throws Exception {
+        Parent page = (Parent) FXMLLoader.load(App.class.getResource(fxml), null, new JavaFXBuilderFactory());
+        Scene scene = stage.getScene();
+        if (scene == null) {
+            scene = new Scene(page, 700, 450);
+            scene.getStylesheets().add(App.class.getResource("demo.css").toExternalForm());
+            stage.setScene(scene);
+        } else {
+            stage.getScene().setRoot(page);
+        }
+        stage.sizeToScene();
+        return page;
+    }
 	
+	
+	public void connectTest() {
+		
+	}
+	public void connectFail(Stage windowFail) {
+		 try {
+	            replaceSceneContent("windowFail.fxml");
+	        } catch (Exception e) {
+	        	e.printStackTrace();
+	        }
+	}
+	/////////////////////////////////////////////////////////////////////
 	public static void main(String[] args) {
 		launch(args);
 	}
