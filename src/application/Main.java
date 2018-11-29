969cd181de27437f342842070d805ee165915038
dlefranc <dlefranc@scinfe2191.univ-pau.fr>
2018-11-29 11:13:45 +0100

    suite

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,26 +6,37 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
 import javafx.scene.text.Font;
 import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.control.*;
 
-import java.util.logging.Level;
+/*import java.util.logging.Level;
 import java.util.logging.Logger;
 import demo.model.User;
-import demo.security.Authenticator;
+import demo.security.Authenticator;*/
 
 public class Main extends Application {
+	private Stage stage;
 	
-	@Override
+	@FXML
+	private TextField userProf;
+	
+	@FXML
+	private TextField mdpProf;
+	
+	
+	/*@Override
 	  public void init() throws Exception {
 	    super.init();
 	 
 	    Font.loadFont(Main.class.getResource("resources/fonts/GreatVibes.otf").toExternalForm(), 12);
-	  }
+	  }*/
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			stage = primaryStage;
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("windowProf.fxml"));
 			Scene scene = new Scene(root,1280,1024);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -37,11 +48,11 @@ public class Main extends Application {
 	}
 	//////////////////////////////////////////////////////////////////
 	private Parent replaceSceneContent(String fxml) throws Exception {
-        Parent page = (Parent) FXMLLoader.load(App.class.getResource(fxml), null, new JavaFXBuilderFactory());
+        Parent page = (Parent) FXMLLoader.load(Main.class.getResource(fxml), null, new JavaFXBuilderFactory());
         Scene scene = stage.getScene();
         if (scene == null) {
-            scene = new Scene(page, 700, 450);
-            scene.getStylesheets().add(App.class.getResource("demo.css").toExternalForm());
+            scene = new Scene(page, 1280, 1024);
+            scene.getStylesheets().add(Main.class.getResource("application.css").toExternalForm());
             stage.setScene(scene);
         } else {
             stage.getScene().setRoot(page);
@@ -50,16 +61,22 @@ public class Main extends Application {
         return page;
     }
 	
-	
-	public void connectTest() {
-		
-	}
 	public void connectFail(Stage windowFail) {
 		 try {
 	            replaceSceneContent("windowFail.fxml");
 	        } catch (Exception e) {
 	        	e.printStackTrace();
 	        }
+	}
+	
+
+	public void connectTest() {
+		if(userProf.getText() == "test" && mdpProf.getText() == "s") {
+			System.out.println("réussi");
+		}
+		else {
+			System.out.println("fail");
+		}
 	}
 	/////////////////////////////////////////////////////////////////////
 	public static void main(String[] args) {
