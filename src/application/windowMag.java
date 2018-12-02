3cffe40720085d6e9326c30905e7d82c31e635c6
batim <batim@DESKTOP-3PI6Q1D>
2018-12-02 01:38:38 +0100

    sweet

---
+++ src/application/windowMag.java
@@ -0,0 +1,21 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.control.*;
+
+public class windowMag {
+
+	public void testMag() {
+		
+		System.out.println("réussi");
+	
+	}
+	
+}
