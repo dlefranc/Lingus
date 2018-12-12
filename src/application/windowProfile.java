c7ff2b4a0db97e67ed7d7364eca0eac824cf452c
batim <batim@DESKTOP-3PI6Q1D>
2018-12-12 22:42:13 +0100

    param

--- src/application/windowProfile.java
+++ src/application/windowProfile.java
@@ -1,12 +1,23 @@
 package application;
 
+import java.io.IOException;
 
-
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class windowProfile extends func{
 	
-	
+	public void viewParam (ActionEvent event) throws IOException{
+		Parent paramView = FXMLLoader.load(getClass().getResource("windowParam.fxml"));
+		Scene paramScene = new Scene(paramView);
+		
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(paramScene);
+	}
 	
 	
 	
