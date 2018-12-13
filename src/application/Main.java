2efce4c2db3221ff51f53519dfd832126a06eab3
dlefranc <dlefranc@scinfe131.univ-pau.fr>
2018-12-13 12:28:22 +0100

    vire font

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,65 +1,23 @@
 package application; 
 	
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 import java.net.MalformedURLException;
 
 import javafx.application.Application;
 
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
-import javafx.scene.text.Font;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 
-/*import java.util.logging.Level;
-import java.util.logging.Logger;
-import demo.model.User;
-import demo.security.Authenticator;*/
 
 public class Main extends Application {
-	//Scene conec, home, mag, workshop, tools, prof, deco;
-	/*private Stage stage;
-	
-	@FXML
-	private TextField userProf;
-	
-	@FXML
-	private TextField mdpProf;*/
-	@FXML
-	private Label labTest;
-	
-	/*@Override
-	  public void init() throws Exception {
-	    super.init();
-	 
-	    Font.loadFont(Main.class.getResource("resources/fonts/GreatVibes.otf").toExternalForm(), 12);
-	  }*/
-	
-	
 	
 	@Override
 	public void start(Stage primaryStage) throws MalformedURLException{
 		
 		try {
-			//stage = primaryStage;
-			//Pane root = (Pane)FXMLLoader.load(getClass().getResource("test.fxml"));
-			//Font.loadFont(Main.class.getResource("../resources/fonts/CANDARA.TFF").toExternalForm(), 30);
-			 
-			
-			      // load a custom font from a specific location (change path!)
-			      // 12 is the size to use
-			     // public Font customFont = Font.loadFont(new FileInputStream(new File("src/resources/fonts/CANDARA.TTF").toURL().toString()), 26);
-			     // labTest.setFont(f); // use this font with our label
-			    
-			
-			
-			 
-			//Font.loadFont(getClass().getResourceAsStream("../resources/fonts/CANDARA.TFF"), 14);
 			Pane root = (Pane)FXMLLoader.load(getClass().getResource("windowParam.fxml"));
 			
 			Scene scene = new Scene(root,1280,1024);
@@ -71,58 +29,6 @@ public class Main extends Application {
 		}
 	}
 	
-	/*public Font getCustomFont() {
-		return customFont;
-	}*/
-	
-	
-	
-	
-	/*long mTime = System.currentTimeMillis();
-	long end = mTime + 5000; // 5 seconds 
-
-	while (System.currentTimeMillis() > end) 
-	{*/
-	      //close this stage  
-	//} 
-	
-	
-	/*PauseTransition delay = new PauseTransition(Duration.seconds(5));
-	delay.setOnFinished( event -> stage.close() );
-	delay.play();*/
-	//////////////////////////////////////////////////////////////////
-	/*private Parent replaceSceneContent(String fxml) throws Exception {
-        Parent page = (Parent) FXMLLoader.load(Main.class.getResource(fxml), null, new JavaFXBuilderFactory());
-        Scene scene = stage.getScene();
-        if (scene == null) {
-            scene = new Scene(page, 1280, 1024);
-            scene.getStylesheets().add(Main.class.getResource("application.css").toExternalForm());
-            stage.setScene(scene);
-        } else {
-            stage.getScene().setRoot(page);
-        }
-        stage.sizeToScene();
-        return page;
-    }*/
-	
-	/*public void connectFail(Stage windowFail) {
-		 try {
-	            replaceSceneContent("windowFail.fxml");
-	        } catch (Exception e) {
-	        	e.printStackTrace();
-	        }
-	}*/
-	
-
-	/*public void connectTest() {
-		if(userProf.getText() == "test" && mdpProf.getText() == "s") {
-			System.out.println("réussi");
-		}
-		else {
-			System.out.println("fail");
-		}
-	}*/
-	/////////////////////////////////////////////////////////////////////
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/application/windowParam.java
+++ src/application/windowParam.java
@@ -1,77 +1,12 @@
 package application;
 
 
-
-import java.awt.FontFormatException;
-import java.awt.GraphicsEnvironment;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.text.Font;
-
 public class windowParam extends func{
-	/*@FXML
-	private Label labTest;
 	
-	public void test() {
-		      labTest.setFont(customFont); // use this font with our label
-		   
-	}*/
-	String salut;
-	String url;
-	Font customFont;
-	@FXML
-	Label labTest;
-	@FXML
-	Button btn;
-	
-	public void initialize() throws MalformedURLException{
-		
-		salut = new File("src/resources/fonts/CANDARA.tff").toURL().toString();
-		System.out.println(salut.substring(5));
-		
-		//url = "../src/resources/fonts/CANDARA.tff";
-		
-		/*try {
-			GraphicsEnvironment ge =
-					GraphicsEnvironment.getLocalGraphicsEnvironment();
-			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, url));
-		}catch (IOException|FontFormatException e) {
-			
-		}*/
-		
-		//labTest.setFont(Font.createFont(Font.TRUETYPE_FONT, url));
 		
 		
-		
-		 Font myFontloadFontAirstreamNF20 = 
-				 
-		            Font.loadFont(getClass().getResourceAsStream(salut.substring(5)), 26);
-		 System.out.println(myFontloadFontAirstreamNF20.getSize());
-		
-		        //btn.setFont(myFontloadFontAirstreamNF20);
-		
-		//System.out.println(url);
-		//System.out.println(url.toString()).toExternalForm());
-		//windowParam.class.getName();
-		
-		//Font.createFont(26, new File("src/resources/fonts/CANDARA.TFF"));
-		
-		//System.out.println(this.getClass().getResource(url.toString()).toExternalForm());
-		
-		
-		//Font.loadFont(windowParam.class.getResource(url.toString()).toExternalForm(), 26);
-		
-		
-	}
-	
-	
 }
+	
+	
+
 
